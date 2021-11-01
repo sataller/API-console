b36ab4f230851e7a01ad6a024ce15b34b35b70d8
sataller <astaller@yandex.ru>
2021-11-01 18:17:37 +0300

    [WIP] add validation message, loader

--- src/components/LoginPage/Login.tsx
+++ src/components/LoginPage/Login.tsx
@@ -1,30 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import {useFormik} from "formik";
-import logo from '../../assets/icons/logo.svg'
+import logo from '../../assets/icons/logo.svg';
+import errorIcon from '../../assets/icons/error-icon.svg';
+import loader from '../../assets/icons/loader.svg'
 
 const Login = () => {
+    const [isFetching, setIsFetching] = useState<boolean>(false);
+    const [isError, setIsError] = useState<boolean>(true);
+
+
     const onSubmit = () => {
+        setIsFetching(true)
+
+        setTimeout(()=>{
+            setIsFetching(false)
+        }, 5000);
         console.log("test")
     }
+    const errorText = '{id: "error/auth/failed", explain: "wrong_credentials"}'
 
     return (
         <Wrapper>
             <LogoStyled src={logo} alt="logo"/>
             <Form onSubmit={onSubmit}>
                 <Title>API-консолька</Title>
-                <Label htmlFor={"login"}> Login</Label>
-                <Input id={"login"} type="text" placeholder="Login"/>
+                {isError && <ErrorBlock>
+                    <SubTitle>
+                        <img src={errorIcon} alt={'error icon'}/>
+                        Вход не вышел
+                    </SubTitle>
+                    <span>{errorText}</span>
+                </ErrorBlock>}
+                <Label error={true} htmlFor={"login"}> Login</Label>
+                <Input error={true} id={"login"} type="text" placeholder="Login"/>
                 <Label htmlFor={"sublogin"}>Sublogin</Label>
                 <Input id={"sublogin"} type="text" placeholder="Sublogin"/>
-                <Label htmlFor={"password"}>Password</Label>
-                <Input id={"password"} type="password" placeholder="Password"/>
+                <Label error={false} htmlFor={"password"}>Password</Label>
+                <Input error={false} id={"password"} type="password" placeholder="Password"/>
                 {/*<input value={login} onChange={(e) => setLogin(e.target.value)} placeholder="Логин" />*/}
                 {/*<input value={sublogin} onChange={(e) => setSubLogin(e.target.value)} placeholder="Сублогин" />*/}
                 {/*<input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Сублогин" />*/}
                 <Button type="submit" onClick={onSubmit}>
-                    Отправить
+                    { !isFetching && "Отправить"}
+                    {isFetching && <img src={loader} alt={"loader"}/>}
+
                 </Button>
+
             </Form>
         </Wrapper>
     );
@@ -42,6 +64,26 @@ const Title = styled.h1`
   margin: 0;
 `;
 
+const SubTitle = styled.h2`
+  display: flex;
+  align-items: center;
+  font-family: SF Pro Text;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 30px;
+  color: #CF2C00;
+  margin: 0;
+  & img{
+    width: 20px;
+    height: 20px  ;
+    margin-right: 10px;
+    margin-left: 2px;
+  }
+  
+ 
+`;
+
 
 const Wrapper = styled.div`
   display: flex;
@@ -55,37 +97,79 @@ const Wrapper = styled.div`
 
 const Form = styled.section`
   display: flex;
-  flex-direction: column;
   width: 520px;
-  height: 425px;
+  flex-direction: column;
   background: #ffffff;
   box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
   border-radius: 5px;
   padding: 40px 30px;
+  @media(max-width: 550px){
+    width: 300px;
+  }
 `;
 
-const Label = styled.label`
+const ErrorBlock = styled.div`
+  max-width: 460px;
+  height: 70px;
+  background: rgba(207, 44, 0, 0.1);
+  border-radius: 5px;
+  padding: 10px;
+  margin-top: 20px;
+  & span { 
+    font-family: SF Pro Text;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 20px;
+    display: flex;
+    align-items: center;
+    text-align: right;
+    color: #CF2C00;
+    opacity: 0.5;
+    margin-left: 32px;
+    
+    @media(max-width: 550px){
+      margin: 0;
+      font-size: 10px;
+    }
+  }
+
+  animation: viewError 1s linear;
+  @keyframes viewError {
+    from{
+      opacity: 0;
+    }
+    to{
+      opacity: 1;
+    }
+  }
+`;
+
+const Label = styled.label<{error?:boolean}>`
   font-family: SF Pro Text;
   font-style: normal;
   font-weight: normal;
   font-size: 16px;
   line-height: 20px;
   margin: 20px 0 5px 0;
+  
+  color: ${props => props.error ? " #CF2C00" : " #0D0D0D"};
 `;
 
-const Input = styled.input`
-  width: 460px;
-  height: 40px;
+const Input = styled.input<{error?:boolean}>`
+  width: 100%;
+  min-height: 40px;
   background: #FFFFFF;
-  border: 1px solid rgba(0, 0, 0, 0.2);
+  border: 1px solid ${props => props.error ? "#CF2C00" : "rgba(0, 0, 0, 0.2)"};
   box-sizing: border-box;
   border-radius: 5px;
   padding: 5px 10px;
+  box-shadow: ${props => props.error ? " 0px 0px 5px rgba(207, 44, 0, 0.5)" : "unset"};
 `;
 
 const Button = styled.button`
-  width: 110px;
-  height: 40px;
+  max-width: 110px;
+  min-height: 40px;
   background: linear-gradient(180deg, #45A6FF 0%, #0055FB 100%), #C4C4C4;
   border-radius: 5px;
   margin-top: 20px;
