8bb473503e1e323231cb1289c41299560372fb1a
astaller <astaller@yandex.ru>
2021-11-03 15:29:46 +0300

    [WIP] refactoring Login component

---
+++ src/utils/validation.ts
@@ -0,0 +1,32 @@
+export const validatePassword = (password: string) => {
+  let error;
+
+  if (!/[A-Z]+/.test(password)) {
+    error = 'There must be one capital letter';
+  }
+  if (!/[0-9]+/.test(password)) {
+    error = 'there must be at least one digit';
+  }
+  if (!/[a-z]/.test(password)) {
+    error = 'There must be one lowercase letter';
+  }
+  if (/[а-я]/.test(password)) {
+    error = 'Can\'t use Cyrillic';
+  }
+  if (password.length < 8) {
+    error = 'Password must be longer than 8 сharacters';
+  }
+  if (!password) {
+    error = 'Required';
+  }
+  return error;
+};
+
+export const validateLogin = (login: string) => {
+  let error;
+
+  if (!login) {
+    error = 'Required';
+  }
+  return error;
+};
