64b15818fbf370de82dd3a5ab7d2d80defa2a0ac
astaller <astaller@yandex.ru>
2021-11-03 13:23:12 +0300

    [WIP] add validation, fix login api, add logOut api

--- src/api/api.ts
+++ src/api/api.ts
@@ -8,28 +8,50 @@ export type LoginPayloadType = {
 }
 
 export enum Status {
-  ERROR = "ERROR",
+  ERROR = 'ERROR',
   OK = 'OK',
 }
 
 export const logIn = async (payload: LoginPayloadType) => {
-  const sendsay = new Sendsay();
-  try {
-    const response = await sendsay.login(payload);
-    return {data: response.json(), status: Status.OK};
-  } catch (error) {
+  const sendsay = new Sendsay({
+    auth: {
+      ...payload
+    }
+  });
 
+  try {
+    await sendsay.login(payload);
+    console.log((sendsay));
+    localStorage.setItem('token', sendsay.session);
+    return {data: sendsay.session, status: Status.OK};
+  } catch (error) {
+    console.log(error);
     return {data: error, status: Status.ERROR};
   }
+};
+
+const logOut = () => {
+  console.log(sendsay);
 }
 
-const test = {
-  id: "error/auth/failed",
-  explain: "wrong_credentials",
-  request: {
-    action: "login",
-    login: "astaller96@gmail.com",
-    sublogin: "",
-    passwd: "ngo04Sepa"
-  }
-}
+// Error response
+// const test = {
+//   id: "error/auth/failed",
+//   explain: "wrong_credentials",
+//   request: {
+//     action: "login",
+//     login: "astaller96@gmail.com",
+//     sublogin: "",
+//     passwd: "ngo04Sepa"
+//   }
+// }
+
+//Ok response
+// {
+//   "request.id": "APP_UNAUTHORIZED_/signin_1635925287823_2021-11-03T10:41:34.060Z",
+//   "sublogin": "x_163585832985503",
+//   "session": "x_163585832985503/x_163585832985503:LL8eT-jwUxvsXMilA280Ugs3afEpFySuagDr0uR1F1rA:1635925295776153944.595757177527637.216944531048",
+//   "duration": 0.732542,
+//   "_ehid": "215578.84122429245.1635925295",
+//   "login": "x_163585832985503"
+// }
--- src/components/LoginPage/Login.tsx
+++ src/components/LoginPage/Login.tsx
@@ -1,50 +1,130 @@
 import React, {useState} from 'react';
-import {Form, Title, Wrapper,} from './LoginStyles';
-import {useFormik} from "formik";
+import {Form, Title, Wrapper} from './LoginStyles';
+import {useFormik} from 'formik';
 import ErrorBlock from './ErrorBlock';
 import CustomInput from './CustomInput';
 import CustomButton from './CustomButton';
 import Logo from '../reusibleComponents/Logo';
 import {logIn, LoginPayloadType, Status} from '../../api/api';
 
+export type FormikValuesType = {
+  login: string
+  sublogin: string
+  password: string
+}
+
+type FormikErrorType = {
+  login?: string
+  password?: string
+}
+
 const Login = () => {
   const [isFetching, setIsFetching] = useState<boolean>(false);
   const [isError, setIsError] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null)
-  const form = useFormik<{login:string, sublogin: string, password:string}>({
+  const [error, setError] = useState<string | null>(null);
+  const form = useFormik<FormikValuesType>({
     initialValues: {
       login: '',
       sublogin: '',
       password: '',
-    }, onSubmit: values => {
-      onSubmit(values)
+    },
+    validate: (values) => {
+      const errors: FormikErrorType = {};
+      const loginError =  validateLogin(values.login);
+      const passwordError =  validatePassword(values.password);
+
+      if (loginError){
+        errors.login = loginError
+      }
+      if (loginError){
+        errors.password = passwordError
+
+      }
+      console.log(errors);
+      return errors;
+    },
+    onSubmit: values => {
+      console.log(form);
+      const payload = getPayload(values);
+      onSubmit({...payload});
       // ngoo4Sepa  astaller96@gmail.com
-    }
+    },
+
   });
+
+  const validateLogin = (login: string) => {
+    let error ;
+
+    if (!login) {
+      error = 'Required';
+    }
+    return error;
+  };
+
+  const validatePassword = (password: string) => {
+    let error;
+
+    if (!/[A-Z]+/.test(password)) {
+      error = 'There must be one capital letter';
+    }
+    if (!/[0-9]+/.test(password)) {
+      error = 'there must be at least one digit';
+    }
+    if (!/[a-z]/.test(password)) {
+      error = 'There must be one lowercase letter';
+    }
+    if (/[а-я]/.test(password)) {
+      error = 'Can\'t use Cyrillic';
+    }
+    if (password.length < 8) {
+      error = 'Password must be longer than 8 сharacters';
+    }
+    if (!password) {
+      error = 'Required';
+    }
+    return error;
+  };
+  const getPayload = (values: any) => {
+    const payload: {[key: string]: string} = {};
+    for (let key in values) {
+
+      if (values[key] !== '') {
+        payload[key] = values[key];
+      }
+    }
+    return JSON.parse(JSON.stringify(payload));
+  };
+
   const onSubmit = async (payload: LoginPayloadType) => {
+    debugger
     setError(null);
     setIsFetching(true);
     const response = await logIn(payload);
 
     if (response.status === Status.ERROR) {
-      setError(`{id: ${response?.data?.id}, explain: ${response?.data?.explain}`)
+      setError(`{id: ${response?.data?.id}, explain: ${response?.data?.explain}`);
     }
+    console.log(response);
     setIsFetching(false);
   };
-  const errorText = '{id: "error/auth/failed", explain: "wrong_credentials"}';
 
   return (
     <Wrapper>
-      <Logo/>
+      <Logo />
       <Form onSubmit={form.handleSubmit}>
         <Title>API-консолька</Title>
         {error && (
-          <ErrorBlock errorText={errorText}/>
+          <ErrorBlock errorText={error} />
         )}
-        <CustomInput key={'login'} error={true} id={'login'} placeholder='Login'/>
-        <CustomInput key={'sublogin'} id={'sublogin'} placeholder='Sublogin'/>
-        <CustomInput key={'password'} error={false} id={'password'} placeholder='Password'/>
-        <CustomButton onSubmit={form.handleSubmit} isFetching={isFetching} text={'Send'}/>
+        <CustomInput key={'login'} error={(form.touched.login && form.errors.login) || ''} id={'login'}
+                     onChange={form.handleChange}
+                     value={form.values.login} placeholder='Login' type={'text'} />
+        <CustomInput key={'sublogin'} id={'sublogin'} placeholder='Sublogin' onChange={form.handleChange}
+                     value={form.values.sublogin} type={'text'} />
+        <CustomInput key={'password'} error={(form.touched.password && form.errors.password) || ''} id={'password'}
+                     placeholder='Password' onChange={form.handleChange}
+                     value={form.values.password} type={'password'} />
+        <CustomButton onSubmit={form.handleSubmit} isFetching={isFetching} text={'Send'} />
       </Form>
     </Wrapper>
   );
