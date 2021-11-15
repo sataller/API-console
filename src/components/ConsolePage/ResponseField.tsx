82539f1aeefedb50fadaa679c2a0455e0da2da6b
astaller <astaller@yandex.ru>
2021-11-15 11:30:10 +0300

    [WIP] fix bugs

--- src/components/ConsolePage/ResponseField.tsx
+++ src/components/ConsolePage/ResponseField.tsx
@@ -18,13 +18,13 @@ export default ResponseField;
 const ResponseFieldWrapper = styled.div<{width?: number; error?: boolean}>`
   resize: none;
   min-width: 400px;
-  max-height: 95%;
+  height: 95%;
   background: #ffffff;
   border: 1px solid ${(props) => (props.error ? '#CF2C00' : 'rgba(0, 0, 0, 0.2)')};
   border-radius: 5px;
   padding: 10px;
   ${(props) => props.error && 'box-shadow: 0px 0px 5px rgba(207, 44, 0, 0.5);'};
-  overflow: scroll;
+  overflow: auto;
   & div > pre {
     margin: 0;
   }
